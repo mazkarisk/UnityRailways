c20abf42c2ca3c3d5b57b80f7ccbc39f539dc0fe
mazkarisk <108807611+mazkarisk@users.noreply.github.com>
2025-07-26 21:52:38 +0900

    SideSurface機能開発

--- Assets/Mazkarisk/UnityRailways/Train/ProcedualMesh.cs
+++ Assets/Mazkarisk/UnityRailways/Train/ProcedualMesh.cs
@@ -4,95 +4,10 @@ using UnityEngine;
 public class ProcedualMesh {
 
 	/// <summary>
-	/// 板部材の端部のメッシュの作成
-	/// </summary>
-	/// <param name="thickness">板厚(m単位)</param>
-	/// <param name="height">高さ(m単位)</param>
-	/// <param name="length">長さ(m単位)</param>
-	/// <param name="endRound">端面のR加工幅(m単位)</param>
-	/// <param name="sideRound">端面以外のR加工幅(m単位)</param>
-	/// <returns></returns>
-	public static Mesh PlateEnd(float thickness, float height, float length, float endRound, float sideRound) {
-		Mesh mesh = new Mesh();
-
-		float angle = Mathf.PI / 4;
-		float x0 = -thickness / 2;
-		float x1 = -thickness / 2 + sideRound;
-		float x2 = thickness / 2 - sideRound;
-		float x3 = thickness / 2;
-		float y0 = 0;
-		float y1 = sideRound;
-		float y2 = height - sideRound;
-		float y3 = height;
-		float z0 = -thickness / 2 * Mathf.Sin(angle);
-		float z1 = 0;
-		float z2 = length - endRound;
-		float z3 = length;
-
-		// 頂点バッファの作成
-		List<Vector3> vertices = Get3x3GridVertices(x0, x1, x2, x3, y0, y1, y2, y3, z0, z1, z2, z3);
-
-		// UVバッファの作成
-		List<Vector2> uvs = new List<Vector2>();
-		// -X面
-		int k = 0;
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].z, vertices[k].y));
-		}
-		// +X面
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].z, vertices[k].y));
-		}
-		// -Y面
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].x, vertices[k].z));
-		}
-		// +Y面
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].x, vertices[k].z));
-		}
-		// -Z面
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].x, vertices[k].y));
-		}
-		// +Z面
-		for (int i = 0; i < 16; i++, k++) {
-			uvs.Add(new Vector2(vertices[k].x, vertices[k].y));
-		}
-
-		// 頂点の法線バッファの作成
-		List<Vector3> normals = Get3x3GridNormals(vertices);
-
-		// 頂点の位置の調整
-		for (int i = 0; i < vertices.Count; i++) {
-			Vector3 vertex = vertices[i];
-			if (vertex.z == z0) {
-				vertices[i] = new Vector3(vertex.x * Mathf.Cos(angle), vertex.y, vertex.z);
-			}
-		}
-
-		// インデックスバッファの作成
-		List<int> indices = new List<int>();
-		for (int i = 0; i < 6; i++) {
-			indices.AddRange(GetGridIndices(3, 3, i * 16));
-		}
-
-		// メッシュの作成
-		mesh.SetVertices(vertices);
-		mesh.SetNormals(normals);
-		mesh.SetUVs(0, uvs);
-		mesh.SetIndices(indices, MeshTopology.Triangles, 0);
-		mesh.RecalculateBounds();
-		mesh.RecalculateTangents();
-
-		return mesh;
-	}
-
-	/// <summary>
 	/// 3×3の格子状に整列したメッシュで構成された直方体の頂点バッファを作成する。
 	/// </summary>
 	/// <returns>作成した頂点バッファ</returns>
-	private static List<Vector3> Get3x3GridVertices(float x0, float x1, float x2, float x3, float y0, float y1, float y2, float y3, float z0, float z1, float z2, float z3) {
+	public static List<Vector3> Get3x3GridVertices(float x0, float x1, float x2, float x3, float y0, float y1, float y2, float y3, float z0, float z1, float z2, float z3) {
 		List<Vector3> vertices = new List<Vector3> {
 			// -X面
 			new Vector3(x0, y0, z3), new Vector3(x0, y0, z2), new Vector3(x0, y0, z1), new Vector3(x0, y0, z0),
@@ -137,7 +52,7 @@ public class ProcedualMesh {
 	/// 3×3の格子状に整列したメッシュで構成された直方体の法線バッファを作成する。
 	/// </summary>
 	/// <returns>作成した頂点バッファ</returns>
-	private static List<Vector3> Get3x3GridNormals(List<Vector3> vertices) {
+	public static List<Vector3> Get3x3GridNormals(List<Vector3> vertices) {
 
 		// 各座標の最小値・最大値の算出
 		float x0 = float.PositiveInfinity;
@@ -183,13 +98,69 @@ public class ProcedualMesh {
 	}
 
 	/// <summary>
+	/// 3×3の格子状に整列したメッシュで構成された直方体のUVバッファを作成する。
+	/// </summary>
+	/// <returns>作成したUVバッファ</returns>
+	public static List<Vector2> Get3x3GridUVs(List<Vector3> vertices, Vector3 offset) {
+		List<Vector2> uvs = new List<Vector2>();
+
+		// -X面
+		int k = 0;
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(-vertices[k].z, vertices[k].y));
+		}
+		// +X面
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(vertices[k].z, vertices[k].y));
+		}
+		// -Y面
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(-vertices[k].x, vertices[k].z));
+		}
+		// +Y面
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(-vertices[k].x, -vertices[k].z));
+		}
+		// -Z面
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(-vertices[k].x, -vertices[k].y));
+		}
+		// +Z面
+		for (int i = 0; i < 16; i++, k++) {
+			Vector3 vertex = vertices[k] + offset;
+			uvs.Add(new Vector2(-vertices[k].x, vertices[k].y));
+		}
+
+		return uvs;
+	}
+
+	/// <summary>
+	/// ×3の格子状に整列したメッシュで構成された直方体のインデックスバッファを作成する。
+	/// </summary>
+	/// <returns>作成したインデックスバッファ</returns>
+	public static List<int> Get3x3GridIndices() {
+		List<int> indices = new List<int>();
+		
+		for (int i = 0; i < 6; i++) {
+			indices.AddRange(GetGridIndices(3, 3, i * 16));
+		}
+
+		return indices;
+	}
+
+	/// <summary>
 	/// 格子状に整列したメッシュのインデックスバッファを作成する。
 	/// </summary>
 	/// <param name="gridSizeX">格子の横サイズ(四角形の数)</param>
 	/// <param name="gridSizeY">格子の縦サイズ(四角形の数)</param>
 	/// <param name="offset">インデックスのオフセット</param>
 	/// <returns>作成したインデックスバッファ</returns>
-	private static List<int> GetGridIndices(int gridSizeX, int gridSizeY, int offset) {
+	public static List<int> GetGridIndices(int gridSizeX, int gridSizeY, int offset) {
 		List<int> indices = new List<int>();
 
 		for (int y = 0; y < gridSizeY; y++) {
--- Assets/Mazkarisk/UnityRailways/Train/SideSurface.cs
+++ Assets/Mazkarisk/UnityRailways/Train/SideSurface.cs
@@ -1,22 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[ExecuteAlways]
 public class SideSurface : MonoBehaviour {
-	private Mesh mesh = null;
 
 	/// <summary>
 	/// 板厚(m単位)
 	/// </summary>
-	[SerializeField, Tooltip("板厚(m単位)")]
+	[SerializeField, Tooltip("板厚(m単位)"), Range(0.001f, 0.1f)]
 	float plateThickness = 0.004f;
+
 	/// <summary>
 	/// 端面(板同士の接合部)のR加工半径(m単位)
 	/// </summary>
-	[SerializeField, Tooltip("端面(板同士の接合部)のR加工半径(m単位)")]
+	[SerializeField, Tooltip("端面(板同士の接合部)のR加工半径(m単位)"), Range(0.0001f, 0.1f)]
 	float plateEndRound = 0.0005f;
 	/// <summary>
 	/// 側面(板同士の接合部以外)のR加工半径(m単位)
 	/// </summary>
-	[SerializeField, Tooltip("側面(板同士の接合部以外)のR加工半径(m単位)")]
+	[SerializeField, Tooltip("側面(板同士の接合部以外)のR加工半径(m単位)"), Range(0.0001f, 0.1f)]
 	float plateSideRound = 0.001f;
 
 	/// <summary>
@@ -62,38 +65,215 @@ public class SideSurface : MonoBehaviour {
 	[SerializeField, Tooltip("前方の開口部の上部から上の高さ(m単位)")]
 	float frontApertureTopHeight = 0.75f;
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	private bool refreshMeshesRequired = true;
+	private Mesh rearBottomMesh = null;
+	private Mesh rearTopMesh = null;
+	private Mesh midMesh = null;
+	private Mesh frontBottomMesh = null;
+	private Mesh frontTopMesh = null;
+
 	void Start() {
-		Mesh rearBottomMesh = ProcedualMesh.PlateEnd(plateThickness, rearApertureBottomHeight, rearApertureLength, plateEndRound, plateSideRound);
+		RefreshMeshes();
+		refreshMeshesRequired = false;
+	}
+
+	void OnValidate() {
+		refreshMeshesRequired = true;
+	}
+
+	void Update() {
+		// 必要に応じてメッシュを初期化する
+		if (refreshMeshesRequired) {
+			RefreshMeshes();
+			refreshMeshesRequired = false;
+		}
+	}
+
+	private void RefreshMeshes() {
+		rearBottomMesh = NegativeZEndMesh(plateThickness, rearApertureBottomHeight, rearApertureLength, plateEndRound, plateSideRound);
 		transform.Find("RearBottomPart").GetComponent<MeshFilter>().mesh = rearBottomMesh;
-		transform.Find("RearBottomPart").SetLocalPositionAndRotation(new Vector3(0, 0, rearApertureLength), Quaternion.Euler(0, 180, 0));
+		transform.Find("RearBottomPart").SetLocalPositionAndRotation(new Vector3(0, 0, rearApertureLength), Quaternion.identity);
 
-		Mesh rearTopMesh = ProcedualMesh.PlateEnd(plateThickness, rearApertureTopHeight, rearApertureLength, plateEndRound, plateSideRound);
+		rearTopMesh = NegativeZEndMesh(plateThickness, rearApertureTopHeight, rearApertureLength, plateEndRound, plateSideRound);
 		transform.Find("RearTopPart").GetComponent<MeshFilter>().mesh = rearTopMesh;
-		transform.Find("RearTopPart").SetLocalPositionAndRotation(new Vector3(0, overallHeight - rearApertureTopHeight, rearApertureLength), Quaternion.Euler(0, 180, 0));
+		transform.Find("RearTopPart").SetLocalPositionAndRotation(new Vector3(0, overallHeight - rearApertureTopHeight, rearApertureLength), Quaternion.identity);
 
-		Mesh frontBottomMesh = ProcedualMesh.PlateEnd(plateThickness, frontApertureBottomHeight, frontApertureLength, plateEndRound, plateSideRound);
+		frontBottomMesh = PositiveZEndMesh(plateThickness, frontApertureBottomHeight, frontApertureLength, plateEndRound, plateSideRound);
 		transform.Find("FrontBottomPart").GetComponent<MeshFilter>().mesh = frontBottomMesh;
 		transform.Find("FrontBottomPart").SetLocalPositionAndRotation(new Vector3(0, 0, overallLength - frontApertureLength), Quaternion.identity);
 
-		Mesh frontTopMesh = ProcedualMesh.PlateEnd(plateThickness, frontApertureTopHeight, frontApertureLength, plateEndRound, plateSideRound);
+		frontTopMesh = PositiveZEndMesh(plateThickness, frontApertureTopHeight, frontApertureLength, plateEndRound, plateSideRound);
 		transform.Find("FrontTopPart").GetComponent<MeshFilter>().mesh = frontTopMesh;
 		transform.Find("FrontTopPart").SetLocalPositionAndRotation(new Vector3(0, overallHeight - rearApertureTopHeight, overallLength - frontApertureLength), Quaternion.identity);
 
+		midMesh = PillarMesh();
+		transform.Find("MidPart").GetComponent<MeshFilter>().mesh = midMesh;
+		transform.Find("MidPart").SetLocalPositionAndRotation(new Vector3(0, rearApertureBottomHeight, rearApertureLength), Quaternion.identity);
 	}
 
-	// Update is called once per frame
-	void Update() {
+	/// <summary>
+	/// 板部材の-Z方向の端部のメッシュの作成
+	/// </summary>
+	/// <param name="thickness">板厚(m単位)</param>
+	/// <param name="height">高さ(m単位)</param>
+	/// <param name="length">長さ(m単位)</param>
+	/// <param name="endRound">端面のR加工幅(m単位)</param>
+	/// <param name="sideRound">端面以外のR加工幅(m単位)</param>
+	/// <returns></returns>
+	public static Mesh NegativeZEndMesh(float thickness, float height, float length, float endRound, float sideRound) {
+		Mesh mesh = new Mesh();
 
+		float angle = Mathf.PI / 4;
+		float x0 = -thickness / 2;
+		float x1 = -(thickness / 2 - sideRound);
+		float x2 = thickness / 2 - sideRound;
+		float x3 = thickness / 2;
+		float y0 = 0;
+		float y1 = sideRound;
+		float y2 = height - sideRound;
+		float y3 = height;
+		float z0 = -length;
+		float z1 = -(length - endRound);
+		float z2 = 0;
+		float z3 = thickness / 2 * Mathf.Sin(angle);
+
+		// 頂点バッファの作成
+		List<Vector3> vertices = ProcedualMesh.Get3x3GridVertices(x0, x1, x2, x3, y0, y1, y2, y3, z0, z1, z2, z3);
+
+		// 法線バッファの作成
+		List<Vector3> normals = ProcedualMesh.Get3x3GridNormals(vertices);
+
+		// UVバッファの作成
+		List<Vector2> uvs = ProcedualMesh.Get3x3GridUVs(vertices, Vector3.zero);
+
+		// インデックスバッファの作成
+		List<int> indices = ProcedualMesh.Get3x3GridIndices();
+
+		// 頂点の位置の調整
+		for (int i = 0; i < vertices.Count; i++) {
+			Vector3 vertex = vertices[i];
+			if (vertex.z == z3) {
+				vertices[i] = new Vector3(vertex.x * Mathf.Cos(angle), vertex.y, vertex.z);
+			}
+		}
+
+		// メッシュの作成
+		mesh.SetVertices(vertices);
+		mesh.SetNormals(normals);
+		mesh.SetUVs(0, uvs);
+		mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+		mesh.RecalculateBounds();
+		mesh.RecalculateTangents();
+
+		return mesh;
 	}
 
-	private void OnDrawGizmosSelected() {
+	/// <summary>
+	/// 板部材の+Z方向の端部のメッシュの作成
+	/// </summary>
+	/// <param name="thickness">板厚(m単位)</param>
+	/// <param name="height">高さ(m単位)</param>
+	/// <param name="length">長さ(m単位)</param>
+	/// <param name="endRound">端面のR加工幅(m単位)</param>
+	/// <param name="sideRound">端面以外のR加工幅(m単位)</param>
+	/// <returns></returns>
+	public static Mesh PositiveZEndMesh(float thickness, float height, float length, float endRound, float sideRound) {
+		Mesh mesh = new Mesh();
 
-		Gizmos.color = new Color(1, 0, 0, 1);
+		float angle = Mathf.PI / 4;
+		float x0 = -thickness / 2;
+		float x1 = -(thickness / 2 - sideRound);
+		float x2 = thickness / 2 - sideRound;
+		float x3 = thickness / 2;
+		float y0 = 0;
+		float y1 = sideRound;
+		float y2 = height - sideRound;
+		float y3 = height;
+		float z0 = -thickness / 2 * Mathf.Sin(angle);
+		float z1 = 0;
+		float z2 = length - endRound;
+		float z3 = length;
 
-		Mesh rearBottomMesh = ProcedualMesh.PlateEnd(plateThickness, rearApertureBottomHeight, rearApertureLength, plateEndRound, plateSideRound);
-		Gizmos.DrawWireMesh(rearBottomMesh, transform.TransformPoint(Vector3.zero), transform.rotation, Vector3.one);
-		Mesh rearTopMesh = ProcedualMesh.PlateEnd(plateThickness, rearApertureTopHeight, rearApertureLength, plateEndRound, plateSideRound);
-		Gizmos.DrawWireMesh(rearTopMesh, transform.TransformPoint(new Vector3(0, overallHeight - rearApertureTopHeight, 0)), transform.rotation, Vector3.one);
+		// 頂点バッファの作成
+		List<Vector3> vertices = ProcedualMesh.Get3x3GridVertices(x0, x1, x2, x3, y0, y1, y2, y3, z0, z1, z2, z3);
+
+		// 法線バッファの作成
+		List<Vector3> normals = ProcedualMesh.Get3x3GridNormals(vertices);
+
+		// UVバッファの作成
+		List<Vector2> uvs = ProcedualMesh.Get3x3GridUVs(vertices, Vector3.zero);
+
+		// インデックスバッファの作成
+		List<int> indices = ProcedualMesh.Get3x3GridIndices();
+
+		// 頂点の位置の調整
+		for (int i = 0; i < vertices.Count; i++) {
+			Vector3 vertex = vertices[i];
+			if (vertex.z == z0) {
+				vertices[i] = new Vector3(vertex.x * Mathf.Cos(angle), vertex.y, vertex.z);
+			}
+		}
+
+		// メッシュの作成
+		mesh.SetVertices(vertices);
+		mesh.SetNormals(normals);
+		mesh.SetUVs(0, uvs);
+		mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+		mesh.RecalculateBounds();
+		mesh.RecalculateTangents();
+
+		return mesh;
+	}
+
+	/// <summary>
+	/// 板部材の柱部のメッシュの作成
+	/// </summary>
+	/// <returns></returns>
+	public Mesh PillarMesh() {
+		Mesh mesh = new Mesh();
+
+		float angle = Mathf.PI / 4;
+		float x0 = -plateThickness / 2;
+		float x1 = -(plateThickness / 2 - plateSideRound);
+		float x2 = plateThickness / 2 - plateSideRound;
+		float x3 = plateThickness / 2;
+		float y0 = -plateThickness / 2 * Mathf.Sin(angle);
+		float y1 = 0;
+		float y2 = overallHeight - rearApertureBottomHeight - rearApertureTopHeight;
+		float y3 = overallHeight - rearApertureBottomHeight - rearApertureTopHeight + plateThickness / 2 * Mathf.Sin(angle);
+		float z0 = 0;
+		float z1 = plateSideRound;
+		float z2 = overallLength - rearApertureLength - frontApertureLength - plateSideRound;
+		float z3 = overallLength - rearApertureLength - frontApertureLength;
+
+		// 頂点バッファの作成
+		List<Vector3> vertices = ProcedualMesh.Get3x3GridVertices(x0, x1, x2, x3, y0, y1, y2, y3, z0, z1, z2, z3);
+
+		// 法線バッファの作成
+		List<Vector3> normals = ProcedualMesh.Get3x3GridNormals(vertices);
+
+		// UVバッファの作成
+		List<Vector2> uvs = ProcedualMesh.Get3x3GridUVs(vertices, Vector3.zero);
+
+		// インデックスバッファの作成
+		List<int> indices = ProcedualMesh.Get3x3GridIndices();
+
+		// 頂点の位置の調整
+		for (int i = 0; i < vertices.Count; i++) {
+			Vector3 vertex = vertices[i];
+			if (vertex.y == y0 || vertex.y == y3) {
+				vertices[i] = new Vector3(vertex.x * Mathf.Cos(angle), vertex.y, vertex.z);
+			}
+		}
+
+		// メッシュの作成
+		mesh.SetVertices(vertices);
+		mesh.SetNormals(normals);
+		mesh.SetUVs(0, uvs);
+		mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+		mesh.RecalculateBounds();
+		mesh.RecalculateTangents();
+
+		return mesh;
 	}
 }
