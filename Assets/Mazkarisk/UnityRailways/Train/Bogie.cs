a44397600e25824ce909759e3a336ad00fbde941
mazkarisk <108807611+mazkarisk@users.noreply.github.com>
2025-10-06 20:59:42 +0900

    歯車箱の回転数で制御する方式に変更、操作方法も後進操作を切り替える形に変更

--- Assets/Mazkarisk/UnityRailways/Train/Bogie.cs
+++ Assets/Mazkarisk/UnityRailways/Train/Bogie.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class Bogie : MonoBehaviour {
@@ -14,7 +15,8 @@ public class Bogie : MonoBehaviour {
 	public Vector3 averagedLinearVelocity { get; private set; } = Vector3.zero;
 	public Vector3 averagedAngularVelocity { get; private set; } = Vector3.zero;
 
-	public float torque { get; set; } = 0f;
+	public int notch { get; set; } = 0;
+	public bool backward { get; set; } = false;
 
 	void FixedUpdate() {
 		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
@@ -45,12 +47,32 @@ public class Bogie : MonoBehaviour {
 		// トルクの設定
 		Wheelset wheelsetF = goWheelsetF.GetComponent<Wheelset>();
 		Wheelset wheelsetR = goWheelsetR.GetComponent<Wheelset>();
-		Rigidbody axelRigidbodyF = wheelsetF.getAxelC().GetComponent<Rigidbody>();
-		Rigidbody axelRigidbodyR = wheelsetR.getAxelC().GetComponent<Rigidbody>();
-		axelRigidbodyF.maxAngularVelocity = float.PositiveInfinity;
-		axelRigidbodyR.maxAngularVelocity = float.PositiveInfinity;
-		axelRigidbodyF.AddRelativeTorque(new Vector3(torque, 0, 0), ForceMode.Force);
-		axelRigidbodyR.AddRelativeTorque(new Vector3(torque, 0, 0), ForceMode.Force);
+		HingeJoint hingeJointF = wheelsetF.getGearCase().GetComponent<HingeJoint>();
+		HingeJoint hingeJointR = wheelsetR.getGearCase().GetComponent<HingeJoint>();
+
+		if (notch == 0) {
+			// 惰行
+			hingeJointF.useMotor = false;
+			hingeJointR.useMotor = false;
+		} else {
+			JointMotor motor = new JointMotor();
+			motor.freeSpin = false;
+			motor.force = 2000f * Mathf.Abs(notch);
+			if (notch > 0) {
+				// 前進
+				motor.targetVelocity = -100000f * (backward ? -1 : 1); // 十分に大きな値
+				hingeJointF.motor = motor;
+				motor.targetVelocity = -motor.targetVelocity;
+				hingeJointR.motor = motor;
+			} else {
+				// ブレーキ
+				motor.targetVelocity = 0f;
+				hingeJointF.motor = motor;
+				hingeJointR.motor = motor;
+			}
+			hingeJointF.useMotor = true;
+			hingeJointR.useMotor = true;
+		}
 	}
 
 	private struct BogieData {
